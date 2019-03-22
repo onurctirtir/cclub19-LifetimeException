fb023ce7e31c4ee35f2d49d728f7653478a63522
Onur TIRTIR <onurcantirtir@gmail.com>
2019-03-22 22:23:55 +0300

    A is accepted

--- a.cpp
+++ a.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <list>
 
+int main() {
+    std::list<char> s;
+    auto it = s.begin();
+
+    std::string input;
+
+    std::getline(std::cin, input);
+
+    for (char c : input) {
+
+        if (c == '[') {
+            it = s.begin();
+        }
+        else if (c == ']') {
+            it = s.end();
+        }
+        else {
+            if (it != s.end()) {
+                std::next(it);
+            }
+
+            if (it == s.end()) {
+                s.push_back(c);
+                it = s.end();
+            }
+            else {
+                s.insert(it, c);
+            }
+        }
+    }
+
+    for (char it : s) {
+        std::cout << it;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
